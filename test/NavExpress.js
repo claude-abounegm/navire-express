8ba78b52d83652f6f717d8b60a02547d2c14aada
Claude Abounegm <claude.abounegm@gmail.com>
2020-02-02 21:45:28 +0200

    adding tests

--- test/NavExpress.js
+++ test/NavExpress.js
@@ -4,7 +4,18 @@ const NavExpress = require("../src");
 const { expect } = require("chai");
 
 describe("NavExpress", function() {
-  it("should work", function() {
-    console.log();
+  it("using middleware", function() {
+    const middleware = NavExpress.init({ props: { title: "App Title" } }, [
+      { type: "link", title: "Link1", href: "/link1" }
+    ]);
+
+    const req = {};
+    const res = {};
+    middleware(req, res, e => {
+      const { nav } = res;
+
+      expect(nav.props.title).equal("App Title");
+      expect(nav.findByHref("/link1")).to.not.be.false;
+    });
   });
 });
