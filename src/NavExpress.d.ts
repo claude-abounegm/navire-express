a634b1fc15a976f73f144d5aec9fb9a22acc444b
Claude Abounegm <claude.abounegm@gmail.com>
2020-02-02 20:01:12 +0200

    fix to typings

--- src/NavExpress.d.ts
+++ src/NavExpress.d.ts
@@ -1,11 +1,19 @@
-import e from "express";
-import Nav from "../Nav";
+import express from "express";
+import Nav from "navire";
 
 declare class NavExpress {
   static init<PropsType>(
     opts?: Nav.CtorOpts<PropsType>,
     init?: Nav.Init<PropsType>
-  ): e.Handler;
+  ): express.Handler;
 }
 
 export = NavExpress;
+
+declare global {
+  namespace Express {
+    interface Response {
+      nav: Nav;
+    }
+  }
+}
